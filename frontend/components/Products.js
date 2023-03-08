665c9027de9a29fe26cf53489d5b7bb9ad4096b4
xedsvg <vlad@redcodenetwork.ro>
2023-03-08 19:37:11 +0200

    fix: subcategory view

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,20 +1,42 @@
-import React, { useState } from "react";
-import { View, Heading } from "native-base";
+import React, { useState, useEffect } from "react";
+import { View, Heading, HStack, ScrollView } from "native-base";
 
 import Product from "./Product";
 
 const Products = ({ category, products, addToCart, removeFromCart }) => {
-  const [ message, setMessage ] = useState(`What ${category} would you like?`);
+  const [message, setMessage] = useState(`What ${category} would you like?`);
+  const [localProducts, setLocalProducts] = useState([]);
+  const [localSubCategories, setLocalSubCategories] = useState([]);
 
   const addToCartAndChangeMessage = (product) => {
     addToCart(product);
     setMessage(`Excellent! Any other ${category}?`);
-  }
-  
+  };
+
   const removeFromCartAndChangeMessage = (product) => {
     removeFromCart(product);
     setMessage(`Less is more! Any other ${category}?`);
-  }
+  };
+
+  const extractProductsBasedOnCategory = () => {
+    const prod = products.filter((product) => product.category === category);
+    setLocalProducts(prod);
+    extractSubCategories(prod);
+  };
+
+  const extractSubCategories = (localProducts) => {
+    const localSubCat = new Set();
+    localProducts.map((product) => {
+      localSubCat.add(product.subCategory);
+    });
+    setLocalSubCategories([...localSubCat]);
+  };
+
+
+  useEffect(()=> {
+     extractProductsBasedOnCategory();
+  }, [])
+
   return (
     <View
       justifyContent="space-evenly"
@@ -26,18 +48,30 @@ const Products = ({ category, products, addToCart, removeFromCart }) => {
       <Heading marginBottom="2rem" marginTop="1rem" color="black" bold>
         {message}
       </Heading>
+     
+      <ScrollView w="full" h="80vh">
+      {localSubCategories.map((subCategory) => (
+        <View key={subCategory || 'Other'}>
+          <Heading size="sm" marginBottom="2rem" marginTop="1rem" color="black" bold>
+            { subCategory || 'Other'}
+          </Heading>
+          <HStack>
+          {localProducts.map((product) => {
+            if (product.subCategory === subCategory)
+              return (
+                <Product
+                  key={product._id}
+                  product={product}
+                  addToCart={addToCartAndChangeMessage}
+                  removeFromCart={removeFromCartAndChangeMessage}
+                />
+              );
+          })}
+          </HStack>
+        </View>
+      ))}
+      </ScrollView>
 
-      {products.map((product) => {
-        if (product.category === category)
-          return (
-            <Product
-              key={product._id}
-              product={product}
-              addToCart={addToCartAndChangeMessage}
-              removeFromCart={removeFromCartAndChangeMessage}
-            />
-          );
-      })}
     </View>
   );
 };
