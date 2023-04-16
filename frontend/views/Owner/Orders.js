bb4a972a8027045851842f8a8f1fa4d936040044
xedsvg <vlad@redcodenetwork.ro>
2023-04-16 15:23:08 +0300

    fix: Owner orders view css

--- frontend/views/Owner/Orders.js
+++ frontend/views/Owner/Orders.js
@@ -1,163 +1,156 @@
 import React from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Button } from "native-base";
+
+const getItemsWithQuantities = (items) => {
+    const itemsMap = {};
+
+    items.forEach((item) => {
+        if (itemsMap[item.name]) {
+            itemsMap[item.name]++;
+        } else {
+            itemsMap[item.name] = 1;
+        }
+    });
+
+    return Object.entries(itemsMap).map(([name, quantity]) => ({ name, quantity }));
+};
 
 export default function Order({ orders, changeStatusHandler }) {
-  return (
-    <View>
-      <Text
-        style={{
-          alignSelf: "center",
-          marginBottom: 40,
-          fontSize: 16,
-          color: "black",
-        }}
-      >
-        {orders.length > 0 ? "Current orders" : "No orders"}
-      </Text>
-      {orders.length > 0 ? (
+    const groupedOrders = {};
+    orders.forEach((order) => {
+        const tableId = order.tabId.tableId;
+        if (groupedOrders[tableId]) {
+            groupedOrders[tableId].push(order);
+        } else {
+            groupedOrders[tableId] = [order];
+        }
+    });
+
+    const sortedOrders = Object.values(groupedOrders).flatMap((orders) =>
+        orders.sort((a, b) => a.tabId.tableId.localeCompare(b.tabId.tableId))
+    );
+
+    return (
         <View>
-          {orders.map((item) => {
-            return (
-              <View
-                style={{
-                  backgroundColor: "#ECECEC",
-                  borderRadius: 10,
-                  marginHorizontal: 20,
-                  marginBottom: 20,
-                }}
-                key={item._id}
-              >
-                <Text
-                  style={{
-                    marginLeft: 0,
-                    fontSize: 22,
-                    marginTop: 10,
-                    color: "black",
-                    textAlign: "center",
-                  }}
-                >
-                  Table No. {item.tabId.tableNo}
-                </Text>
-                <View
-                  style={{
-                    borderBottomWidth: 1,
-                    borderBottomColor: "black",
-                    marginHorizontal: 90,
-                    marginTop: 10,
-                  }}
-                />
+            {!sortedOrders.length && <Text style={styles.title}>
+                "No orders"
+            </Text>}
+            {sortedOrders.length &&
+                <View style={styles.orders}>
+                    {sortedOrders.map((item) => {
+                        const items = getItemsWithQuantities(item.items);
 
-                <View
-                  style={{
-                    backgroundColor: "#C8C8C8",
-                    borderRadius: 10,
-                    marginTop: 20,
-                    marginHorizontal: 10,
-                  }}
-                >
-                  <Text
-                    style={{
-                      marginLeft: 10,
-                      fontSize: 16,
-                      marginTop: 10,
-                      color: "black",
-                    }}
-                  >
-                    All Products:-{" "}
-                  </Text>
-                  <Text
-                    style={{
-                      marginLeft: 10,
-                      fontSize: 14,
-                      lineHeight: 22,
-                      marginTop: 10,
-                      color: "black",
-                    }}
-                  >
-                    {item.items.map((e) => {
-                      return <Text key={e._id}>{e.name + "\n"}</Text>;
+                        return (
+                            <View style={styles.container} key={item._id}>
+                                <View>
+                                <Text style={styles.tableNo}>
+                                    Table Nr: 2
+                                </Text>
+                                <View style={styles.separator} />
+                                </View>
+
+                                <View style={styles.items}>
+                                    {items.map(({ name, quantity }) => (
+                                        <Text key={name} style={styles.product}>
+                                            {quantity}x {name}
+                                        </Text>
+                                    ))}
+                                </View>
+
+                                <View>
+                                <View style={styles.total}>
+                                    <Text style={styles.label}>Total:</Text>
+                                    <Text style={styles.totalAmount}>{item.totalAmount}</Text>
+                                </View>
+
+                                <Button
+                                    style={styles.button}
+                                    onPress={async () => {
+                                        changeStatusHandler(item._id, "inProgress");
+                                    }}
+                                >
+                                    Start Preparing 🍽️
+                                </Button>
+                                </View>
+                            </View>
+                        );
                     })}
-                  </Text>
                 </View>
-
-                <View
-                  style={{
-                    backgroundColor: "#C8C8C8",
-                    borderRadius: 10,
-                    marginTop: 20,
-                    marginHorizontal: 10,
-                  }}
-                >
-                  <Text
-                    style={{
-                      marginLeft: 10,
-                      fontSize: 16,
-                      marginTop: 10,
-                      color: "black",
-                    }}
-                  >
-                    Order Total
-                  </Text>
-                  <Text
-                    style={{
-                      marginLeft: 10,
-                      fontSize: 16,
-                      marginTop: 10,
-                      color: "green",
-                      marginBottom: 10,
-                    }}
-                  >
-                    {item.totalAmount}
-                  </Text>
-                </View>
-
-                <TouchableOpacity
-                  style={{
-                    paddingTop: 10,
-                    marginTop: 10,
-                    marginBottom: 20,
-                    margin: 10,
-                  }}
-                  onPress={async () => {
-                    changeStatusHandler(item._id, "inProgress");
-                  }}
-                >
-                  <View
-                    style={{
-                      backgroundColor: "#222222",
-                      padding: 12,
-                      borderRadius: 6,
-                    }}
-                  >
-                    <Text
-                      style={{
-                        color: "white",
-                        textAlign: "center",
-                      }}
-                    >
-                      Start Preparing 🍽️
-                    </Text>
-                  </View>
-                </TouchableOpacity>
-              </View>
-            );
-          })}
+            }
         </View>
-      ) : null}
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  button: {
-    backgroundColor: "#222222",
-    padding: 12,
-    borderRadius: 6,
-  },
+    orders: {
+        display: 'grid',
+        gridTemplateColumns: 'repeat(5, 1fr)',
+        gap: 5
+    },
+    title: {
+        alignSelf: "center",
+        marginBottom: 40,
+        fontSize: 16,
+        color: "black",
+        justifySelf: "flex-start",
+    },
+    container: {
+        backgroundColor: "#fff",
+        borderRadius: 10,
+        marginBottom: 20,
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    tableNo: {
+        marginLeft: 0,
+        fontSize: 22,
+        marginTop: 10,
+        color: "black",
+        textAlign: "center",
+    },
+    separator: {
+        borderBottomWidth: 1,
+        borderBottomColor: "black",
+        marginHorizontal: 90,
+        marginTop: 10,
+        justifySelf: "flex-start",
+    },
+    items: {
+        borderRadius: 10,
+        marginTop: 20,
+        marginHorizontal: 10,
+        padding: 10
+    },
+    product: {
+        fontSize: 14
+    },
+    total: {
+        padding: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        borderRadius: 10,
+        marginTop: 20,
+        marginHorizontal: 10,
+        paddingHorizontal: 10,
+    },
+    label: {
+        fontSize: 16,
+        color: "black",
+    },
+    totalAmount: {
+        fontSize: 16,
+        color: "green",
+        marginLeft: 10,
+    },
+    button: {
+        paddingTop: 10,
+        marginTop: 10,
+        marginBottom: 20,
+        margin: 10,
+        backgroundColor: "#222222",
+        padding: 12,
+        borderRadius: 6,
+    }
 });
--- frontend/views/Owner/Owner.js
+++ frontend/views/Owner/Owner.js
@@ -11,6 +11,7 @@ export default function Owner({restaurantId}) {
     
     const fetchOrders = async () => {
         const orders = await (await fetch(`${baseUrl}/orders/active/${restaurantId}`)).json();
+        
         await setOrders(orders);
     }
 
@@ -25,31 +26,7 @@ export default function Owner({restaurantId}) {
 
     return (
         <View>
-        <View style={{ flex: 1, justifyContent: 'center', padding: 24, marginTop: 30, marginBottom: 20}}>
-                <View style={{ flexDirection: 'row', height: 55, backgroundColor: "#ECECEC", borderRadius: 8 }}>
-                            
-                        <TouchableOpacity
-                            style={{ 
-                                flex: 1,
-                                backgroundColor: viewMode === "orders" ? "#999999" : null, 
-                                margin: 5,
-                                borderRadius: 8,
-                            }}
-                            onPress={() => setViewMode("orders")}
-                        >
-                            <View
-                                    style={{
-                                        flex: 1,
-                                        flexDirection: 'row',
-                                        alignItems: 'center',
-                                        justifyContent: 'center'
-                                    }}
-                                >
-                                <Text style={{ marginLeft: 0, fontSize: 16,  lineHeight: 22, color: "black" }}>Orders</Text>
-                            </View>
-                        </TouchableOpacity>
-                    </View>
-                </View>  
+        
                 {viewMode === "orders" && <Orders orders={orders} changeStatusHandler={changeStatusHandler} />}
         </View>
     );
